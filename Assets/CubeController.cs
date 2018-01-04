b50aee823479c046e6a7e6620d21762fa266c25a
TeppeiYamada <te322y@gmail.com>
2018-01-05 00:18:38 +0900

    GoForwardUnityChan初めてのコミット
    課題が終了しました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private float speed = -0.2f;
+
+	private float deadLine = -10;
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (this.speed, 0, 0);
+
+		if(transform.position.x < deadLine){
+
+			Destroy (gameObject);
+
+		}
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		if(other.gameObject.tag == "CubeTag"){
+
+			audioSource.Play ();
+
+		}
+
+		if(other.gameObject.tag == "GroundTag"){
+
+			audioSource.Play ();
+
+		}
+
+	}
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+	Animator animator;
+
+	Rigidbody2D rigid2d;
+
+	private float groundLevel = -3.0f;
+
+	private float dump = 0.8f;
+
+	float jumpVelocity = 20;
+
+	private float deadLine = -9;
+
+	// Use this for initialization
+	void Start () {
+
+		this.animator = GetComponent<Animator> ();
+
+		this.rigid2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.animator.SetFloat ("Horizontal", 1);
+
+		bool isGround = (transform.position.y > groundLevel) ? false : true;
+		this.animator.SetBool ("isGround", isGround);
+
+		this.GetComponent<AudioSource> ().volume = (isGround) ? 1 : 0;
+
+		if (Input.GetMouseButtonDown(0) && isGround){
+
+			this.rigid2d.velocity = new Vector2 (0, jumpVelocity);
+
+		}
+
+		if (Input.GetMouseButton(0) == false){
+
+			if(this.rigid2d.velocity.y > 0){
+				
+				this.rigid2d.velocity *= this.dump;
+
+			}
+
+		}
+
+		if(transform.position.x < deadLine){
+
+			GameObject.Find ("Canvas").GetComponent<UIController> ().GameOver();
+
+			Destroy (gameObject);
+
+		}
+		
+	}
+}
